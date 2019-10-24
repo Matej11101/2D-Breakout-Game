5236f36ef0742c24ea99cee1d62180c2b1c77ac7
Mateusz_Ch <476594@chrome.escg.ac.uk>
2019-10-24 16:29:11 +0100

    Ball and paddle movement

--- components.js
+++ components.js
@@ -1,6 +1,7 @@
 /////// LOAD IMAGES ////////
 
-
+const BG_IMG = new Image();
+BG_IMG.src = "img/bg.jpg";
 
 /////// END LOAD IMAGES ////////
 
--- game.js
+++ game.js
@@ -12,6 +12,9 @@ ctx.lineWidth = 3;
 const PADDLE_WIDTH = 100;
 const PADDLE_MARGIN_BOTTOM = 50;
 const PADDLE_HEIGHT = 20;
+const BALL_RADIUS = 8;
+let leftArrow = false;
+let rightArrow = false;
 
 //CREATE THE PADDLE
 const paddle = {
@@ -31,16 +34,71 @@ function drawPaddle(){
     ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
     
 }
+
+document.addEventListener("keydown", function(event){
+    if(event.keyCode == 37){
+        leftArrow = true;
+}else if(event.keyCode == 39){
+        rightArrow = true;
+    }
+});
+    
+document.addEventListener("keyup", function(event){
+    if(event.keyCode == 37){
+    leftArrow = false;
+}else if(event.keyCode == 39){
+    rightArrow = false;
+    }
+});
+
+    
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < cvs.width){
+       paddle.x += paddle.dx; 
+    }else if (leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+
+const ball = {
+    x : cvs.width/2,
+    y : paddle.y - BALL_RADIUS,
+    radius : BALL_RADIUS,
+    speed : 4,
+    dx : 3,
+    dy : -3
+}
+function drawBall(){
+    ctx.beginPath();
+    
+    ctx.arc(ball.x,ball.y,ball.radius,0,Math.PI*2);
+    ctx.fillStyle = "#ffcd05";
+    ctx.fill();
+    
+    ctx.strokeStyle = "#2e3548";
+    ctx.stroke();
+    
+    ctx.closePath();
+}
+
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
 function draw(){
     drawPaddle();
+    drawBall();
     
 }
 
 function update(){
-    
+    movePaddle();
+    moveBall();
 }
 
 function loop(){
+    
+    ctx.drawImage(BG_IMG, 0, 0);
     
     draw();
     
